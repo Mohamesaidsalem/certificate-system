0a64c91e76fc5b76f6c82c557ed29f939772f8f4
Mohamesaidsalem <mohamedsaidsalem@outlook.com>
2026-01-11 03:21:32 +0200

    Fix formatting of supabaseKey declaration

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://typabwoqgjxsqwklkgwv.supabase.co'
-const supabaseKey = 'sb_publishable_V88t2EoZPtNXb_sGU7Pucw_ACtLfNGc' // ضع المفتاح هنا
+const supabaseKey = 'sb_publishable_V88t2EoZPtNXb_sGU7Pucw_ACtLfNGc'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
